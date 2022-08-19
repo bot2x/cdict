5192aa1cac5248cf02634699ddbea35c2bed4179
Shubham <shubham0921s@gmail.com>
2022-08-19 18:54:43 +0530

    corrects the name and date

--- cdict.c
+++ cdict.c
@@ -1,9 +1,6 @@
 /******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+Author : bot2x
+Date   : 7th Aug, 2022
 *******************************************************************************/
 
 #include <stdio.h>
